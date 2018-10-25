735a77a2fdc8ea6f51f864a94c8676593969866a
Marco Pivetta <ocramius@gmail.com>
2018-10-25 15:52:23 +0200

    Projection that dumps all checked in users in a json file per building id

--- bin/project-checked-in-users.php
+++ bin/project-checked-in-users.php
@@ -3,8 +3,14 @@
 
 declare(strict_types=1);
 
+namespace Projection;
+
+use Building\Domain\Aggregate\Building;
+use Building\Domain\DomainEvent\UserCheckedIn;
+use Building\Domain\DomainEvent\UserCheckedOut;
 use Interop\Container\ContainerInterface;
 use Prooph\EventStore\EventStore;
+use Prooph\EventStore\Stream\StreamName;
 
 (function () {
     /** @var ContainerInterface $container */
@@ -12,16 +18,27 @@ use Prooph\EventStore\EventStore;
 
     $eventStore = $container->get(EventStore::class);
 
+    /** @var array<string, array<string, null>> $users */
     $users = [];
 
-    foreach ($eventStore->??? as $event) {
-        // ...
+    foreach ($eventStore->loadEventsByMetadataFrom(
+        new StreamName('event_stream'),
+        ['aggregate_type' => Building::class]
+    ) as $event) {
+        if ($event instanceof UserCheckedIn) {
+            $users[$event->buildingId()->toString()][$event->username()] = null;
+        }
 
-        // TODO TODO TODO
+        if ($event instanceof UserCheckedOut) {
+            unset($users[$event->buildingId()->toString()][$event->username()]);
+        }
+    }
+
+    array_walk($users, function (array $usernames, string $buildingId) : void {
         file_put_contents(
             __DIR__ . '/../public/building-' . $buildingId . '.json',
-            json_encode($users[$buildingId])
+            json_encode(array_keys($usernames))
         );
-    }
+    });
 })();
 
